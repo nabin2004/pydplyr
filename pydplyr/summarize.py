7eb33b20e1be3880c75311d28de239d010d14fd3
nabin2004 <olinabin2004@gmail.com>
2024-07-26 01:20:42 +0545

    Added the simple summarize function

--- pydplyr/summarize.py
+++ pydplyr/summarize.py
@@ -1,8 +1,13 @@
-def summarize():
+def summarize(df):
+    """A simple summarize function that info about the dataframe.
+
+    Args:
+        df: A pandas DataFrame
+
+    Returns:
+        Info about the dataframe
     """
-    A simple summarize function that prints a message when called.
-    """
-    return "This is a summarize function"
+    return df.info()
 
 
 if __name__ == "__main__":
