ba735b3eefffcd316683b8ee4ab02113eb5ffaa2
Nabin Oli <olinabin2004@gmail.com>
2024-10-26 23:03:03 +0545

    Made ready for initial draft deployment

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Nabin\Desktop\MY WORK ZONE\Projects\pydplyr\pydplyr\myenv
+set VIRTUAL_ENV=C:\Users\whoami\Desktop\CLONE\pydplyr\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
