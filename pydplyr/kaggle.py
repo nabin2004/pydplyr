4c81b406d954290b274e07c3b504c5599c20b415
Nabin Oli <olinabin2004@gmail.com>
2024-11-08 14:17:41 +0545

    Added the missing_data() function in kaggle.py

---
+++ pydplyr/kaggle.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd 
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+def missing_data(data):
+    total = data.isnull().sum()
+    percent = (data.isnull().sum()/data.isnull().count()*100)
+    tt = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+    types = []
+    for col in data.columns:
+        dtype = str(data[col].dtype)
+        types.append(dtype)
+    tt['Types'] = types
+    return(np.transpose(tt))
+
+def missing_data(data):
+    total = data.isnull().sum()
+    percent = (data.isnull().sum()/data.isnull().count()*100)
+    tt = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+    types = []
+    for col in data.columns:
+        dtype = str(data[col].dtype)
+        types.append(dtype)
+    tt['Types'] = types
+    return(np.transpose(tt))
+
+def plot_feature_distribution(df1, df2, label1, label2, features):
+    i = 0
+    sns.set_style('whitegrid')
+    plt.figure()
+    fig, ax = plt.subplots(10,10,figsize=(18,22))
+
+    for feature in features:
+        i += 1
+        plt.subplot(10,10,i)
+        sns.distplot(df1[feature], hist=False,label=label1)
+        sns.distplot(df2[feature], hist=False,label=label2)
+        plt.xlabel(feature, fontsize=9)
+        locs, labels = plt.xticks()
+        plt.tick_params(axis='x', which='major', labelsize=6, pad=-6)
+        plt.tick_params(axis='y', which='major', labelsize=6)
+    plt.show();
+
+def plot_new_feature_distribution(df1, df2, label1, label2, features):
+    i = 0
+    sns.set_style('whitegrid')
+    plt.figure()
+    fig, ax = plt.subplots(2,4,figsize=(18,8))
+
+    for feature in features:
+        i += 1
+        plt.subplot(2,4,i)
+        sns.kdeplot(df1[feature], bw=0.5,label=label1)
+        sns.kdeplot(df2[feature], bw=0.5,label=label2)
+        plt.xlabel(feature, fontsize=11)
+        locs, labels = plt.xticks()
+        plt.tick_params(axis='x', which='major', labelsize=8)
+        plt.tick_params(axis='y', which='major', labelsize=8)
+    plt.show();
