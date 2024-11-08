3a4eb1b0aa6b0b2d8b3cf9fd7291919968096328
Nabin Oli <olinabin2004@gmail.com>
2024-11-08 14:18:47 +0545

    Added plot_feature_distribution() dunction

--- pydplyr/kaggle.py
+++ pydplyr/kaggle.py
@@ -14,17 +14,6 @@ def missing_data(data):
     tt['Types'] = types
     return(np.transpose(tt))
 
-def missing_data(data):
-    total = data.isnull().sum()
-    percent = (data.isnull().sum()/data.isnull().count()*100)
-    tt = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
-    types = []
-    for col in data.columns:
-        dtype = str(data[col].dtype)
-        types.append(dtype)
-    tt['Types'] = types
-    return(np.transpose(tt))
-
 def plot_feature_distribution(df1, df2, label1, label2, features):
     i = 0
     sns.set_style('whitegrid')
@@ -42,19 +31,3 @@ def plot_feature_distribution(df1, df2, label1, label2, features):
         plt.tick_params(axis='y', which='major', labelsize=6)
     plt.show();
 
-def plot_new_feature_distribution(df1, df2, label1, label2, features):
-    i = 0
-    sns.set_style('whitegrid')
-    plt.figure()
-    fig, ax = plt.subplots(2,4,figsize=(18,8))
-
-    for feature in features:
-        i += 1
-        plt.subplot(2,4,i)
-        sns.kdeplot(df1[feature], bw=0.5,label=label1)
-        sns.kdeplot(df2[feature], bw=0.5,label=label2)
-        plt.xlabel(feature, fontsize=11)
-        locs, labels = plt.xticks()
-        plt.tick_params(axis='x', which='major', labelsize=8)
-        plt.tick_params(axis='y', which='major', labelsize=8)
-    plt.show();
